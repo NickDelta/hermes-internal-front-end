1d9b298661ff8fd450085122c31797dbb2dec73c
vivian <it21818@hua.gr>
2021-01-14 13:03:14 +0200

    CrudEditor done.

--- src/main/java/org/hua/hermes/backend/entity/ApplicationState.java
+++ src/main/java/org/hua/hermes/backend/entity/ApplicationState.java
@@ -1,11 +1,24 @@
 package org.hua.hermes.backend.entity;
 
 public enum ApplicationState {
-    SUBMITTED,
-    APPROVED,
-    COMPLETED,
-    CANCELED,
-    REJECTED,
-    RESUBMISSION_REQUIRED;
+    SUBMITTED("Submitted"),
+    APPROVED("Approved"),
+    COMPLETED("Completed"),
+    CANCELED("Canceled"),
+    REJECTED("Rejected"),
+    RESUBMISSION_REQUIRED("Resubmission Required");
 
+    String name;
+
+    ApplicationState(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
